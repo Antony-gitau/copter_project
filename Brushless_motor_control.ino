0c735195e31543c5b4dda176cac3a0744371ca2a
Antony M. Gitau <65859953+Antony-gitau@users.noreply.github.com>
2021-09-22 17:19:59 +0300

    Arduino code-brushless motor
    An improvement on angle measurement

--- Brushless_motor_control.ino
+++ Brushless_motor_control.ino
@@ -1,29 +1,28 @@
 //variable declaration
 
-int pot_pin = A0;
-int pot_val = 0;
-int delay_time = 750;
-int toggle_pin = 10;
-int motor_driver
-int angle;
+int pot_pin = A0; // set the ppotentialmeter pin 
+int pot_val = 0; //initiate the value to zero
+int toggle_pin = 10; //set the togglepin to ten
+int angle; //initializing variable angle
+
 
 void setup() {
   // put your setup code here, to run once:
-pinMode(pot_pin, INPUT);
-Serial.begin(9600);
-
-
+pinMode(pot_pin, INPUT); //set the potentialmeter as an input, as we shall use it to control rise and fall
+Serial.begin(9600); //setup the serial monitor to read the angle
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-int pot_val = analogueRead(A0);
-int pwmnoutput= map(pot_val, 0, 1023,0,255);
-analogWrite(motor_driver, pwmoutput);
+  
+int pot_val = analogRead(A0); //read the value of the potentialmeter
+int pwmnoutput= map(pot_val, 0, 1023,0,255); //map the value for pwm output
+analogWrite(pot_pin, pwmoutput); //write the value to the potentialmeter
 
 if (digitalWrite(toggle_pin) == HIGH & angle <= 90){
   Serial.read(angle);
-  
-}
-
+  }
+else ((digitalWrite(toggle_pin) == HIGH & angle >= 90){
+  Serial.read(angle);
+  Serial.print("dangerous!");
 }
